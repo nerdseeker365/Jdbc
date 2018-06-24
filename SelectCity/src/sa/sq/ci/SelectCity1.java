976ddd50058a4695342d0f4e7948ae0feb8d4886
karra sankar <karrasankar09@gmail.com>
2018-06-24 19:43:17 +0530

    JDBC SELECT CITY PROGRAM

---
+++ SelectCity/src/sa/sq/ci/SelectCity1.java
@@ -0,0 +1,40 @@
+package sa.sq.ci;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class SelectCity1 {
+
+	public static void main(String[] args)throws Exception {
+		//reading input values
+		Scanner sc=null;
+		String pcity=null;
+		sc=new Scanner(System.in);
+		System.out.print("Enter city name: ");
+		pcity=sc.nextLine();//gives pcity name
+		pcity="'"+pcity+"'";
+		Connection con=null;
+		Statement st=null;
+		ResultSet rs=null;
+		//register TYPE4 jdbc driver
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		//establish the Connection with Database Software
+		con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:ORCL","scott","tiger");
+        //create JDBC Statement object
+		st=con.createStatement();
+		//execute the Query and process the Query
+		rs=st.executeQuery("SELECT * FROM PERSON WHERE PCITY="+pcity);
+		//print Database table records	
+		while(rs.next()) {
+			System.out.println(rs.getInt(1)+"\t"+rs.getString(2)+"\t"+rs.getString(3));
+		}
+		//close JDBC streams objects
+		con.close();
+		st.close();
+		rs.close();
+	}//main method
+
+}//class
