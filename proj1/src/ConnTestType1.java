53d83824b9d8bc755638382faf5c77d85ab7dc2b
karra sankar <karrasankar158@gmail.com>
2018-06-24 09:51:16 +0530

    type1 jdbc driver connection

---
+++ proj1/src/ConnTestType1.java
@@ -0,0 +1,22 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnTestType1 {
+
+	public static void main(String[] args)throws Exception  {
+		Connection con=null;
+		//register driver class object 
+		sun.jdbc.odbc.JdbcOdbcDriver driver=new sun.jdbc.odbc.JdbcOdbcDriver();
+		//register driver with driver manager service
+		DriverManager.registerDriver(driver);
+		//establish the connection
+		con=DriverManager.getConnection("jdbc:odbc:oradsn","scott","tiger");
+		if(con==null) {
+			System.out.println("Connection is not esatblished");
+		}
+		else {
+			System.out.println("Connection is  esatblished");
+		}
+	}//main method 
+}//class
